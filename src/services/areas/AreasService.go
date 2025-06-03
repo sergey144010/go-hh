678ddf8d0457ad68bdb3086f3459caf23aec18b2
sergey <sergey.i@pssltd.ru>
2025-06-03 17:39:15 +0300

    change

--- src/services/areas/AreasService.go
+++ src/services/areas/AreasService.go
@@ -3,11 +3,12 @@ package AreasService
 import (
 	"net/http"
 	AppRequestService "go-hh/src/services/appRequest"
+	"os"
 )
 
 func RequestGetAreas(accessToken string) (*http.Request, error) {
 	return AppRequestService.RequestGet(
-		"https://api.hh.ru/areas",
+		os.Getenv("API_URI") + "/areas",
 		accessToken,
 	)
 }
